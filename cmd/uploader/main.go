acd623486f6cad60d7dcd4a1ea6bd8e4c5824675
paulo gabriel <pgabrieldeveloper@outlook.com>
2023-03-31 23:27:56 -0300

    feat upload criando upload de arquivos simples

--- cmd/uploader/main.go
+++ cmd/uploader/main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"fmt"
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/credentials"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/s3"
+	"os"
 )
 
 var (
@@ -29,5 +31,20 @@ func init() {
 }
 
 func main() {
+}
 
+func uploadFile(filename string) {
+	fmt.Printf("Iniciando upload do arquivo: %s no bucket: ", filename, bukectKey)
+	f, err := os.Open(filename)
+	if err != nil {
+		fmt.Printf("Erro ao abrir arquivo: %s", filename)
+		return
+	}
+	defer f.Close()
+	_, err = s3Cliente.PutObject(&s3.PutObjectInput{Bucket: aws.String(bukectKey), Key: aws.String(filename), Body: f})
+	if err != nil {
+		fmt.Printf("Erro ao subir arquivo para o s3: %s", filename)
+		return
+	}
+	fmt.Printf("upload do arquivo: %s finalizadado ", filename)
 }
