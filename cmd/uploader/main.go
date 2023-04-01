b3109df5ddb67743804ce82c179255fe1788838e
paulo gabriel <pgabrieldeveloper@outlook.com>
2023-04-01 13:14:22 -0300

    feat adicionando threads e limitando

--- cmd/uploader/main.go
+++ cmd/uploader/main.go
@@ -6,12 +6,15 @@ import (
 	"github.com/aws/aws-sdk-go/aws/credentials"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/s3"
+	"io"
 	"os"
+	"sync"
 )
 
 var (
 	s3Cliente *s3.S3
 	bukectKey string
+	wg        *sync.WaitGroup
 )
 
 func init() {
@@ -19,32 +22,74 @@ func init() {
 		&aws.Config{
 			Region: aws.String("us-east-1"),
 			Credentials: credentials.NewStaticCredentials(
-				"exemplo",
-				"exemplo",
+				"Sua acess key",
+				"sua secret key",
 				"",
 			)})
 	if err != nil {
 		panic(err)
 	}
 	s3Cliente = s3.New(sess)
-	bukectKey = "uploader-bukect-s3"
+	bukectKey = "uploader-bucket-s3"
+	wg = &sync.WaitGroup{}
 }
 
 func main() {
+	controlUpload := make(chan struct{}, 50)
+	errFile := make(chan string, 10)
+	go func() {
+		for {
+			select {
+			case filepath := <-errFile:
+				wg.Add(1)
+				controlUpload <- struct{}{}
+				go uploadFile(filepath, controlUpload, errFile)
+			}
+		}
+	}()
+	dir, err := os.Open("../generator")
+	if err != nil {
+		panic(err)
+	}
+	defer dir.Close()
+	for i := 0; i <= 5; i++ {
+		files, err := dir.ReadDir(1)
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			fmt.Println("Error ao ler o arquivo")
+			continue
+		}
+		wg.Add(1)
+		controlUpload <- struct{}{}
+		go uploadFile(files[0].Name(), controlUpload, errFile)
+	}
+
+	wg.Wait()
+
 }
 
-func uploadFile(filename string) {
-	fmt.Printf("Iniciando upload do arquivo: %s no bucket: ", filename, bukectKey)
-	f, err := os.Open(filename)
+func uploadFile(filename string, controlUpload <-chan struct{}, errFile chan<- string) {
+	defer wg.Done()
+	fmt.Printf("Iniciando upload do arquivo: %s no bucket: %s \n", filename, bukectKey)
+	filePath := "../generator/" + filename
+	f, err := os.Open(filePath)
 	if err != nil {
-		fmt.Printf("Erro ao abrir arquivo: %s", filename)
+		fmt.Printf("Erro ao abrir arquivo: %s\n", filename)
+		fmt.Println(err.Error())
+		<-controlUpload
+		errFile <- filePath
 		return
 	}
 	defer f.Close()
 	_, err = s3Cliente.PutObject(&s3.PutObjectInput{Bucket: aws.String(bukectKey), Key: aws.String(filename), Body: f})
 	if err != nil {
 		fmt.Printf("Erro ao subir arquivo para o s3: %s", filename)
+		<-controlUpload
+		errFile <- filePath
 		return
 	}
 	fmt.Printf("upload do arquivo: %s finalizadado ", filename)
+	<-controlUpload
 }
--- go.mod
+++ go.mod
@@ -2,7 +2,6 @@ module github.com/PGabrielDev/upload-s3
 
 go 1.18
 
-require (
-	github.com/aws/aws-sdk-go v1.44.233 // indirect
-	github.com/jmespath/go-jmespath v0.4.0 // indirect
-)
+require github.com/aws/aws-sdk-go v1.44.233
+
+require github.com/jmespath/go-jmespath v0.4.0 // indirect
